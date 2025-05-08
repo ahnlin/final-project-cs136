e99fb350853144c7da29e2c41f37abf66e40e4c0
26ts22 <ts22@williams.edu>
2025-05-08 13:55:55 -0400

    Files Created

---
+++ QuadtreeInterface.java
@@ -0,0 +1,15 @@
+package fires;
+
+public interface QuadtreeInterface<Item>{
+	
+
+	public abstract boolean isEmpty();
+
+	public abstract int size();
+
+	public abstract void insert(Item);
+
+/*Is this to get a full branch or just one entry or one moniter?*/
+	
+	public abstract Item get(Item);
+
