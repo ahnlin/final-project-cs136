912e97b079b35d180f9bf2c4cdb32bfef42010c4
TzeviS <ts22@williams.edu>
2025-05-11 12:41:46 -0400

    Add files via upload

---
+++ Moniter.java
@@ -0,0 +1,21 @@
+package fires;
+public class Moniter{
+	private String site;
+	private double xcoord;
+	private double ycoord;
+	public Moniter(String site, double xcoord,double ycoord){
+		this.site = site;
+		this.xcoord = xcoord;
+		this.ycoord = ycoord;
+	}
+	@Override
+	public String toString() {
+		return site + "\n" + "(" +xcoord+","+ ycoord+ ")" + "\n" + "-------" + "\n";
+	}
+
+	public static void main(String[] args){
+		Moniter test1 = new Moniter("TestSite",312.2,402.1);
+		System.out.println(test1.toString());
+
+	}
+}
