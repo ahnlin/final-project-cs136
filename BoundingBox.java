55e8e2591e93809b135b938351a4ceee418b4694
Ahn Lin <ahnlin@Ahns-MacBook-Air-100.local>
2025-05-11 19:50:37 -0400

    Beg to implement quadtree

---
+++ BoundingBox.java
@@ -0,0 +1,42 @@
+package fires;
+
+public class BoundingBox{
+// bounding boxes are used to partition the space of the quadtree -- each bounding box is divided into 4 even subsections
+
+	//instance variables 
+	public double maxx; 
+	public double maxy; 
+	public double minx; 
+	public double miny; 
+	public double midx; 
+	public double midy; 
+
+	// constructor 
+	public BoundingBox(double maxx, double maxy, double minx, double miny) {
+		this.maxx = maxx; 
+		this.maxy = maxy; 
+		this.minx = minx; 
+		this.miny = miny; 
+
+		midx = (maxx + minx) / 2; 
+		midy = (maxy + miny) / 2;
+
+	}
+
+	public BoundingBox northwestbox() {
+		return new BoundingBox(midx, maxy, minx, midy); 
+	}
+
+	public BoundingBox northeastbox() {
+		return new BoundingBox(maxx, maxy, midx, midy); 
+	}
+
+	public BoundingBox southwestbox() {
+		return new BoundingBox(midx, midy, minx, miny); 
+	}
+
+	public BoundingBox southeastbox() {
+		return new BoundingBox(maxx, midy, midx, miny); 
+
+	}
+}
--- QuadtreeInterface.java
+++ QuadtreeInterface.java
@@ -2,15 +2,17 @@ package fires;
 
 public interface QuadtreeInterface<Item>{
 	
-
+	// returns true if the quadtree is empty
 	public abstract boolean isEmpty();
 
+	// returns the number of non-empty nodes
 	public abstract int size();
 
-	public abstract void insert(Item);
+	// adds object to quadtree 
+	public abstract void insert(Item obj);
 
 /*Is this to get a full branch or just one entry or one moniter?*/
 	
-	public abstract Item get(Item);
+	public abstract Item get(Item obj);
 
 }
