96a62b549803ed8d7be779b96f20d07a52b1d921
Ahn Lin <ahnlin@Ahns-MacBook-Air-100.local>
2025-05-11 20:43:22 -0400

    compiling

--- QuadtreeInterface.java
+++ QuadtreeInterface.java
@@ -9,10 +9,10 @@ public interface QuadtreeInterface<Item>{
 	public abstract int size();
 
 	// adds object to quadtree 
-	public abstract void insert(Item obj);
+	//public abstract void insert(Item obj);
 
 /*Is this to get a full branch or just one entry or one moniter?*/
 	
-	public abstract Item get(Item obj);
+	//public abstract Item get(Item obj);
 
 }
