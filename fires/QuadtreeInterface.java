e205b77993127a853cd22e5afc684bc71105ca7b
26ts22 <ts22@williams.edu>
2025-05-18 16:26:52 -0400

    commenting

--- fires/QuadtreeInterface.java
+++ fires/QuadtreeInterface.java
@@ -2,15 +2,16 @@ package fires;
 
 public interface QuadtreeInterface<Item>{
 	
-
+	//returns whether or not the quadtree is empty
 	public abstract boolean isEmpty();
 
+	//returns number of items in quadtree
 	public abstract int size();
 
 	// adds object to quadtree 
 	public abstract void insert(Item obj, double xcord, double ycord);
 
-/*Is this to get a full branch or just one entry or one moniter?*/
-	//public abstract List<LeafNode> get(double xcval, double yval);
+	//locates closest object given (x,y)
+	public abstract List<LeafNode> get(double xcval, double yval);
 
 }
